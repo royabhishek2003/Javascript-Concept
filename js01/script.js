f8018071159ef75cfe1afc52b3b81611cd7824ea
royabhishek2003 <kishanabhishek2003@gmail.com>
2026-02-25 00:44:30 +0530

    basics of js and datatype and its conversion

---
+++ js01/script.js
@@ -0,0 +1,86 @@
+
+// console.log("Helllo World");
+
+// string to number 
+
+let str="-100";
+// let num= +str;
+// console.log(typeof num);
+
+// type conversion of string to number 
+// let num=+str;
+// console.log(typeof num);
+// console.log(num);
+// let num0= -str;
+// console.log(typeof num0);
+// console.log(num0);
+// let num1= Number(str);
+// console.log(typeof num1);
+// console.log(num1);
+// let num2= parseInt(str);   // "234abc" => 234 , "234.56abc" => 234
+// console.log(typeof num2);
+// console.log(num2);
+// let num3= parseFloat(str);
+// console.log(typeof num3);
+// console.log(num3);
+// let num4= str*1;
+// console.log(typeof num4);
+// console.log(num4);
+
+// NaN - Not a Number(number data type with invalid number )
+// let str1="abhishek";
+// let num5=+str1;
+// console.log(typeof num5, num5);
+
+// convert number to string 
+
+// let num=100;
+// let str1= String(num);
+// console.log(typeof str1, str1);
+// let str2= num+"";
+// console.log(typeof str2, str2);
+// let str3= num.toString();
+// console.log(typeof str3, str3);
+
+// covert boolean to string
+
+// let bool=true;
+// let str1= String(bool);
+// console.log(typeof str1, str1);
+// let str2= bool+"";
+// console.log(typeof str2, str2);
+// let str3= bool.toString();
+// console.log(typeof str3, str3);
+
+// convert boolean to number 
+
+// let bool= false;
+// let num1= +bool;
+// console.log(typeof num1, num1);
+// let num2= Number(bool);
+// console.log(typeof num2, num2);
+
+// let num3=-bool;
+// console.log(typeof num3, num3);
+// let num4= -Number(bool);
+// console.log(typeof num4, num4);
+
+// undefined 
+
+// console.log(typeof undefined); // undefined
+// console.log(undefined); // undefined
+// let num= +undefined;
+// console.log(typeof num, num); // number NaN
+
+// // difference bwteen null and undefined 
+// console.log(typeof null); // object
+// console.log(null); // null
+// let num1= +null;
+// console.log(typeof num1, num1); // number 0
+
+
+
+
+
+
+ 
